0d72770e0edd075b5f02c41715927d054d8f1e6e
Belyaev Dmitry <belyaev.dmnd@gmail.com>
2025-07-23 13:53:47 +0300

    Create footer.php. Include it to index.php.

---
+++ app/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <div>
+        <a href="tel:+79531401581">Позвонить нам</a>
+        <a href="mailto:belyaev.dmnd@gmail.com">Напишите нам</a>
+        <p>Все права зарегистрированы &copy;</p>
+    </div>
+</footer>
---
+++ app/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="author" content="Belyaev Dmitry">
+        <meta name="description" content="Онлайн калькулятор">
+        <meta name="keywords" content="Калькулятор">
+        <title>Калькулятор</title>
+    </head>
+        <body>
+            
+        </body>
+   <?php 
+   include 'footer.php';
+   ?>
+</html>
