75eded27c1809daf84464b88488d9f5dc3775263
Belyaev Dmitry <belyaev.dmnd@gmail.com>
2025-07-23 14:31:59 +0300

    Creater navbar.php, include it. Create en empty form to index.php

--- app/index.php
+++ app/index.php
@@ -7,9 +7,15 @@
         <title>Калькулятор</title>
     </head>
         <body>
-            
+            <?php include 'navbar.php'; ?>
+            <form action="" method="get">
+                <p>Выбирите математическую функцию вычеслений.</p>
+                <input type="text" disabled><br><br>
+                <input type="text" disabled><br><br>
+                <input type="submit" value="Посчитать">
+                <input type="reset" value="Очистить">
+            </form>
+            <br>
         </body>
-   <?php 
-   include 'footer.php';
-   ?>
+   <?php include 'footer.php'; ?>
 </html>
---
+++ app/navbar.php
@@ -0,0 +1,7 @@
+<nav>
+    <a href="/index.php">Главная</a>
+    <a href="/addition.php">Сложение</a>
+    <a href="/substraction.php">Вычитание<a>
+    <a href="/multiplication.php">Умножение</a>
+    <a href="/division.php">Деление</a>
+</nav>
